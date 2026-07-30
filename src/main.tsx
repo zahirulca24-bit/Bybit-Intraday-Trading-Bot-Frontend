332f590f9a8b339137829ebc4d98bdd18ea8f1d1
Md. Zahirul Islam <zahirul.ca24@gmail.com>
2026-07-31 01:55:00 +0600

    fix: complete frontend control authentication

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
+import { installOperatorSessionFetch } from "./services/operatorSession";
 import "./index.css";
+
+installOperatorSessionFetch();
 
 const rootElement = document.getElementById("root");
 
